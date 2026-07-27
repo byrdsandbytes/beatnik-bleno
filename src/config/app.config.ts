42b78622ddb8fa7b5fd6617a3dd557fd48ed35db
idrimi <gerber@byrds.ch>
2026-07-27 12:54:15 +0200

    adjust colors

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -75,7 +75,7 @@ export const LED_CONFIG = {
   // Pending Action: Pulse yellow to red
   PENDING_ACTION: {
     command: 'pulse',
-    params: { on_color: [1, 0, 0], off_color: [1, 1, 0], fade_in: 1, fade_out: 1 }
+    params: { on_color: [0.2, 0.4, 0.46], off_color: [1, 1, 0], fade_in: 1, fade_out: 1 }
   },
   // Restarting: Blink green twice (we simulate this by blinking fast)
   RESTARTING: {
@@ -85,7 +85,7 @@ export const LED_CONFIG = {
   // Resetting: Blink yellow twice (we simulate this by blinking fast)
   RESETTING: {
     command: 'blink',
-    params: { color: [1, 1, 0], on_time: 0.2, off_time: 0.2 }
+    params: { color: [0.2, 1, 0], on_time: 0.2, off_time: 0.2 }
   }
 } as const;
 
